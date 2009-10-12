f751021f1eaa27d4965e6bf9cd4e4c62eee8a076
Niclas Nilsson <niclas.nilsson@factor10.com>
2009-10-12 17:19:44 +0200

    added clean debug prints

--- spec/meta-utils_spec.rb
+++ spec/meta-utils_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/regular_methods')
 
-describe "MetaUtils" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "dp" do
+  before do
+    @stdout_string = StringIO.new
+    $stdout = @stdout_string
+  end
+  
+  it "should support clean debug prints" do
+    some_method
+    @stdout_string.string.should == "name=\"Niclas\", @age=38, team=[\"Magnus\", \"Fredrik\", \"Jonas\"]\n"
   end
 end
---
+++ spec/regular_methods.rb
@@ -0,0 +1,10 @@
+require "#{File.dirname(__FILE__)}/../lib/meta-utils"
+
+def some_method
+    name = 'Niclas'
+    @age = 38
+    team = ['Magnus', 'Fredrik', 'Jonas']
+    
+    d.p "name, @age, team"
+end
+ 
